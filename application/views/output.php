4a71856fb3f7c42c6b11d7dcc9c682ea0284d6b7
renzodigman-retsolsystems <renzodigman.retsolsystems@gmail.com>
2018-02-20 15:55:27 +0800

    Reads XLS File
    Can now read spreadsheet files and turn them into tables.
    
    Scope: Can only read files inside local folder.

--- application/views/output.php
+++ application/views/output.php
@@ -3,6 +3,21 @@ require 'vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+// use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
+
+$file = $_POST['fileUpload'];
+
+$spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($file);
+
+$xls_data = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
+
+$highestRow = $spreadsheet->getActiveSheet()->getHighestRow();
+$highestColumn = $spreadsheet->getActiveSheet()->GetHighestColumn();
+$highestCol = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::columnIndexFromString($highestColumn);
+
+echo 'Row: '.$highestRow.'<br>';
+echo 'Col: '.$highestCol.' / '.$highestColumn;
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -11,8 +26,16 @@ use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
     <title>PROJECT ONE</title>
   </head>
   <body>
+    <table border='1'>
     <?php
-      echo $_POST['fileUpload'];
+      $htm_tbl = '<tr><th>'.implode('</th><th>', $xls_data[1]).'</th></tr>';
+
+      for($row=2; $row <= $highestRow; $row++)
+        // for($col=1; $col <= $highestCol; $col++)
+          $htm_tbl .= '<tr><td>'.implode('</td><td>', $xls_data[$row]).'</td></tr>';
+
+      echo $htm_tbl;
     ?>
+    </table>
   </body>
 </html>
